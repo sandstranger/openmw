23f244ec38d51cefabf285a2201e548fae96640a
sisah <jirielbunda@seznam.cz>
2021-09-12 14:52:33 -0400

    reenablethis

--- apps/openmw/mwrender/objectpaging.cpp
+++ apps/openmw/mwrender/objectpaging.cpp
@@ -770,7 +770,19 @@ namespace MWRender
         {
             mSceneManager->reinstateRemovedState(group);
             osg::ref_ptr<osg::AlphaFunc> alpha = new osg::AlphaFunc(osg::AlphaFunc::GEQUAL, 128.f / 255.f);
-            group->getOrCreateStateSet()->setAttributeAndModes(alpha.get(), osg::StateAttribute::ON);
+
+            if (resourceSystem->getSceneManager()->getLightingMethod() != SceneUtil::LightingMethod::FFP)
+                group->setCullCallback(new SceneUtil::LightListCallback);
+
+            osg::StateSet* stateset = group->getOrCreateStateSet();
+            stateset->removeAttribute(osg::StateAttribute::MATERIAL);
+            stateset->removeAttribute(osg::StateAttribute::ALPHAFUNC);
+            stateset->removeMode(GL_ALPHA_TEST);
+            stateset->removeAttribute(osg::StateAttribute::BLENDFUNC);
+            stateset->removeMode(GL_BLEND);
+            stateset->setRenderBinToInherit();
+
+            stateset->setAttributeAndModes(alpha.get(), osg::StateAttribute::ON);
             mSceneManager->recreateShaders(group, "groundcover", false, true);
         }
 
