dbc131537ffee010dd50fc9a7cf922f1c4ae1214
sisah <jirielbunda@seznam.cz>
2021-09-18 08:04:29 -0400

    probably buggy grass shaders

--- files/shaders/groundcover_vertex.glsl
+++ files/shaders/groundcover_vertex.glsl
@@ -1,52 +1,40 @@
 #version 120
 
-#if @useUBO
-    #extension GL_ARB_uniform_buffer_object : require
-#endif
+#define PER_PIXEL_LIGHTING @normalMap
 
-#if @useGPUShader4
-    #extension GL_EXT_gpu_shader4: require
-#endif
+#define GRASS
 
-#define GROUNDCOVER
-
-attribute vec4 aOffset;
-attribute vec3 aRotation;
+#include "vertexcolors.glsl"
 
 #if @diffuseMap
 varying vec2 diffuseMapUV;
 #endif
 
 #if @normalMap
-varying vec2 normalMapUV;
 varying vec4 passTangent;
 #endif
 
-// Other shaders respect forcePPL, but legacy groundcover mods were designed to work with vertex lighting.
-// They may do not look as intended with per-pixel lighting, so ignore this setting for now.
-#define PER_PIXEL_LIGHTING @normalMap
-
-varying float euclideanDepth;
+varying float depth;
+#if !@radialFog
 varying float linearDepth;
-
-#if PER_PIXEL_LIGHTING
-varying vec3 passViewPos;
-varying vec3 passNormal;
-#else
-centroid varying vec3 passLighting;
-centroid varying vec3 shadowDiffuseLighting;
 #endif
 
-#include "shadows_vertex.glsl"
-#include "lighting.glsl"
-#include "depth.glsl"
+#if PER_PIXEL_LIGHTING
+varying vec3 passNormal;
+varying vec3 passViewPos;
+#endif
 
+#if !PER_PIXEL_LIGHTING
+    centroid varying vec3 passLighting;
+    #include "lighting.glsl"
+#endif
+
+uniform highp mat4 osg_ViewMatrixInverse;
 uniform float osg_SimulationTime;
-uniform mat4 osg_ViewMatrixInverse;
-uniform mat4 osg_ViewMatrix;
-uniform float windSpeed;
-uniform vec3 playerPos;
-uniform mat4 projectionMatrix;
+
+uniform vec3 windData;
+uniform highp vec3 playerPos;
+attribute float originalCoords;
 
 #if @groundcoverStompMode == 0
 #else
@@ -57,24 +45,27 @@ uniform mat4 projectionMatrix;
     #define STOMP_INTENSITY_LEVEL @groundcoverStompIntensity
 #endif
 
-vec2 groundcoverDisplacement(in vec3 worldpos, float h)
+highp vec4 grassDisplacement(vec3 viewPos, vec4 vertex)
 {
-    vec2 windDirection = vec2(1.0);
-    vec3 footPos = playerPos;
-    vec3 windVec = vec3(windSpeed * windDirection, 1.0);
+    highp float h = originalCoords;
 
-    float v = length(windVec);
-    vec2 displace = vec2(2.0 * windVec + 0.1);
-    vec2 harmonics = vec2(0.0);
+    highp vec4 worldPos = osg_ViewMatrixInverse * vec4(viewPos, 1.0);
 
-    harmonics += vec2((1.0 - 0.10*v) * sin(1.0*osg_SimulationTime + worldpos.xy / 1100.0));
-    harmonics += vec2((1.0 - 0.04*v) * cos(2.0*osg_SimulationTime + worldpos.xy / 750.0));
-    harmonics += vec2((1.0 + 0.14*v) * sin(3.0*osg_SimulationTime + worldpos.xy / 500.0));
-    harmonics += vec2((1.0 + 0.28*v) * sin(5.0*osg_SimulationTime + worldpos.xy / 200.0));
+    highp vec2 WindVec = vec2(windData.x);
 
-    vec2 stomp = vec2(0.0);
+    highp float v = length(WindVec);
+    highp vec2 displace = vec2(2.0 * WindVec + 0.1);
+
+    highp vec2 harmonics = vec2(0.0);
+
+    harmonics.xy += vec2((1.0 - 0.10*v) * sin(1.0*osg_SimulationTime +  worldPos.xy / 1100.0));
+    harmonics.xy += vec2((1.0 - 0.04*v) * cos(2.0*osg_SimulationTime +  worldPos.xy / 750.0));
+    harmonics.xy += vec2((1.0 + 0.14*v) * sin(3.0*osg_SimulationTime +  worldPos.xy / 500.0));
+    harmonics.xy += vec2((1.0 + 0.28*v) * sin(5.0*osg_SimulationTime  +  worldPos.xy / 200.0));
+
+    highp vec2 stomp = vec2(0.0);
 #if STOMP
-    float d = length(worldpos.xy - footPos.xy);
+    highp float d = length(worldPos.xy - playerPos.xy);
 #if STOMP_INTENSITY_LEVEL == 0
     // Gentle intensity
     const float STOMP_RANGE = 50.0; // maximum distance from player that grass is affected by stomping
@@ -89,66 +80,39 @@ vec2 groundcoverDisplacement(in vec3 worldpos, float h)
     const float STOMP_DISTANCE = 60.0;
 #endif
     if (d < STOMP_RANGE && d > 0.0)
-        stomp = (STOMP_DISTANCE / d - STOMP_DISTANCE / STOMP_RANGE) * (worldpos.xy - footPos.xy);
+        stomp = (STOMP_DISTANCE / d - STOMP_DISTANCE / STOMP_RANGE) * (worldPos.xy - playerPos.xy);
 
 #ifdef STOMP_HEIGHT_SENSITIVE
-    stomp *= clamp((worldpos.z - footPos.z) / h, 0.0, 1.0);
+    stomp *= clamp((worldPos.z - playerPos.z) / h, 0.0, 1.0);
 #endif
 #endif
 
-    return clamp(0.02 * h, 0.0, 1.0) * (harmonics * displace + stomp);
-}
+    highp vec4 ret = vec4(0.0);
+    ret.xy += clamp(0.02 * h, 0.0, 1.0) * (harmonics * displace + stomp);
 
-mat4 rotation(in vec3 angle)
-{
-    float sin_x = sin(angle.x);
-    float cos_x = cos(angle.x);
-    float sin_y = sin(angle.y);
-    float cos_y = cos(angle.y);
-    float sin_z = sin(angle.z);
-    float cos_z = cos(angle.z);
+#ifdef STORM_MODE
+    highp vec2 stormDir = vec2(windData.y, windData.z);
 
-    return mat4(
-        cos_z*cos_y+sin_x*sin_y*sin_z, -sin_z*cos_x, cos_z*sin_y+sin_z*sin_x*cos_y, 0.0,
-        sin_z*cos_y+cos_z*sin_x*sin_y, cos_z*cos_x, sin_z*sin_y-cos_z*sin_x*cos_y, 0.0,
-        -sin_y*cos_x, sin_x, cos_x*cos_y, 0.0,
-        0.0, 0.0, 0.0, 1.0);
-}
+    if(stormDir != vec2(0.0) && h > 0.0) {
+        ret.xy += h*stormDir;
+        ret.z -= length(ret.xy)/3.14;
+        ret.z -= sin(osg_SimulationTime * min(h, 150.0) / 10.0) * length(stormDir);
+     }
+#endif
 
-mat3 rotation3(in mat4 rot4)
-{
-    return mat3(
-        rot4[0].xyz,
-        rot4[1].xyz,
-        rot4[2].xyz);
+    return vertex + ret;
 }
 
 void main(void)
 {
-    vec3 position = aOffset.xyz;
-    float scale = aOffset.w;
-
-    mat4 rotation = rotation(aRotation);
-    vec4 displacedVertex = rotation * scale * gl_Vertex;
-
-    displacedVertex = vec4(displacedVertex.xyz + position, 1.0);
-
-    vec4 worldPos = osg_ViewMatrixInverse * gl_ModelViewMatrix * displacedVertex;
-    worldPos.xy += groundcoverDisplacement(worldPos.xyz, gl_Vertex.z);
-    vec4 viewPos = osg_ViewMatrix * worldPos;
+    highp vec4 viewPos = (gl_ModelViewMatrix * gl_Vertex);
+    gl_Position = gl_ModelViewProjectionMatrix * grassDisplacement(viewPos.xyz, gl_Vertex);
 
     gl_ClipVertex = viewPos;
-    euclideanDepth = length(viewPos.xyz);
+    depth = length(viewPos.xyz);
 
-    if (length(gl_ModelViewMatrix * vec4(position, 1.0)) > @groundcoverFadeEnd)
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-    else
-        gl_Position = projectionMatrix * viewPos;
-
-    linearDepth = getLinearDepth(gl_Position.z, viewPos.z);
-
-#if (!PER_PIXEL_LIGHTING || @shadows_enabled)
-    vec3 viewNormal = normalize((gl_NormalMatrix * rotation3(rotation) * gl_Normal).xyz);
+#if !@radialFog
+    linearDepth = gl_Position.z;
 #endif
 
 #if @diffuseMap
@@ -156,21 +120,23 @@ void main(void)
 #endif
 
 #if @normalMap
-    normalMapUV = (gl_TextureMatrix[@normalMapUV] * gl_MultiTexCoord@normalMapUV).xy;
-    passTangent = gl_MultiTexCoord7.xyzw * rotation;
+    passTangent = gl_MultiTexCoord7.xyzw;
 #endif
 
-#if PER_PIXEL_LIGHTING
-    passViewPos = viewPos.xyz;
-    passNormal = rotation3(rotation) * gl_Normal.xyz;
-#else
+vec3 viewNormal = normalize((gl_NormalMatrix * gl_Normal).xyz);
+
+
+#if !PER_PIXEL_LIGHTING
+    vec3 shadowDiffuseLighting;
     vec3 diffuseLight, ambientLight;
     doLighting(viewPos.xyz, viewNormal, diffuseLight, ambientLight, shadowDiffuseLighting);
     passLighting = diffuseLight + ambientLight;
     clampLightingResult(passLighting);
+    passLighting += shadowDiffuseLighting;
 #endif
 
-#if (@shadows_enabled)
-    setupShadowCoords(viewPos, viewNormal);
+#if PER_PIXEL_LIGHTING
+    passViewPos = viewPos.xyz;
+    passNormal = gl_Normal.xyz;
 #endif
 }
