3afa4d6e6f93d8337c2f448d875f7681d0fa00da
sisah <jirielbunda@seznam.cz>
2021-09-19 12:02:41 -0400

    fix pbr shader grass

--- files/shaders/pbr/groundcover_fragment.glsl
+++ files/shaders/pbr/groundcover_fragment.glsl
@@ -1,8 +1,14 @@
 #version 120
 
-#define GRASS
+#if @useUBO
+    #extension GL_ARB_uniform_buffer_object : require
+#endif
 
-#define PER_PIXEL_LIGHTING @normalMap
+#if @useGPUShader4
+    #extension GL_EXT_gpu_shader4: require
+#endif
+
+#define GROUNDCOVER
 
 #if @diffuseMap
 uniform sampler2D diffuseMap;
@@ -11,50 +17,44 @@ varying vec2 diffuseMapUV;
 
 #if @normalMap
 uniform sampler2D normalMap;
+varying vec2 normalMapUV;
 varying vec4 passTangent;
 #endif
 
-#include "helperutil.glsl"
-#include "vertexcolors.glsl"
+// Other shaders respect forcePPL, but legacy groundcover mods were designed to work with vertex lighting.
+// They may do not look as intended with per-pixel lighting, so ignore this setting for now.
+#define PER_PIXEL_LIGHTING @normalMap
 
 varying float depth;
 
-#if !@radialFog
-varying float linearDepth;
-#endif
-
 #if PER_PIXEL_LIGHTING
-varying vec3 passNormal;
 varying vec3 passViewPos;
+varying vec3 passNormal;
+#else
+centroid varying vec3 passLighting;
 #endif
 
-  #include "lighting.glsl"
-
-#if !PER_PIXEL_LIGHTING
-     centroid varying vec3 passLighting;
-#endif
-
+#include "helperutil.glsl"
+#include "shadows_fragment.glsl"
+#include "lighting.glsl"
 #include "alpha.glsl"
 
 void main()
 {
-
-if(@groundcoverFadeEnd != @groundcoverFadeStart)
-    if (depth > @groundcoverFadeEnd)
-        discard;
-
 #if @normalMap
-vec4 normalTex = texture2D(normalMap, diffuseMapUV);
-vec3 normalizedNormal = normalize(passNormal);
-vec3 normalizedTangent = normalize(passTangent.xyz);
-vec3 binormal = cross(normalizedTangent, normalizedNormal) * passTangent.w;
-mat3 tbnTranspose = mat3(normalizedTangent, binormal, normalizedNormal);
-vec3 viewNormal = gl_NormalMatrix * normalize(tbnTranspose * (normalTex.xyz * 2.0 - 1.0));
+    vec4 normalTex = texture2D(normalMap, normalMapUV);
+
+    vec3 normalizedNormal = normalize(passNormal);
+    vec3 normalizedTangent = normalize(passTangent.xyz);
+    vec3 binormal = cross(normalizedTangent, normalizedNormal) * passTangent.w;
+    mat3 tbnTranspose = mat3(normalizedTangent, binormal, normalizedNormal);
+
+    vec3 viewNormal = gl_NormalMatrix * normalize(tbnTranspose * (normalTex.xyz * 2.0 - 1.0));
 #endif
 
 #if @diffuseMap
     gl_FragData[0] = texture2D(diffuseMap, diffuseMapUV);
-	  gl_FragData[0].rgb = SRGBToLinear(gl_FragData[0].rgb);
+	gl_FragData[0].rgb = SRGBToLinear(gl_FragData[0].rgb);
 #else
     gl_FragData[0] = vec4(1.0);
 #endif
@@ -64,20 +64,21 @@ vec3 viewNormal = gl_NormalMatrix * normalize(tbnTranspose * (normalTex.xyz * 2.
 
     alphaTest();
 
+    float shadowing = unshadowedLightRatio(depth);
+
     vec3 lighting;
 #if !PER_PIXEL_LIGHTING
     lighting = passLighting;
 #else
     vec3 diffuseLight, ambientLight;
-    doLighting(passViewPos, normalize(viewNormal), 1.0, diffuseLight, ambientLight);
+    doLighting(passViewPos, normalize(viewNormal), shadowing, diffuseLight, ambientLight);
     lighting = diffuseLight + ambientLight;
     clampLightingResult(lighting);
 #endif
 
-gl_FragData[0].xyz *= lighting;
+    gl_FragData[0].xyz *= lighting * Fd_Lambert();
 
-    highp float fogValue = clamp((depth - gl_Fog.start) * gl_Fog.scale, 0.0, 1.0);
-
+    float fogValue = clamp((depth - gl_Fog.start) * gl_Fog.scale, 0.0, 1.0);
 
 	float exposure = mix(4.6, 2.6, length(SRGBToLinearApprox(lcalcDiffuse(0).xyz) + SRGBToLinearApprox(gl_LightModel.ambient.xyz)) * 0.5);
 	
@@ -97,4 +98,5 @@ gl_FragData[0].xyz *= lighting;
 
     gl_FragData[0].xyz = mix(gl_FragData[0].xyz, gl_Fog.color.xyz, fogValue);
 
+    applyShadowDebugOverlay();
 }
